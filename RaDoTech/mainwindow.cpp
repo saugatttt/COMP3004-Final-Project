ef18218dabeea6e40744eece2c010dfc107a4d50
RamiGolea <rami.golea@gmail.com>
2024-12-04 15:12:39 -0500

    Created user profile managerment, persistence, and adding/deleting functionality

--- RaDoTech/mainwindow.cpp
+++ RaDoTech/mainwindow.cpp
@@ -6,10 +6,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    manager = new UserProfileManager("./users.json", nullptr);
+
+    manager->createUserProfile("Rami", "Golea", "rami.golea@gmail.com", "Male", 20, 66, 173);
+    manager->createUserProfile("Test", "hello", "hi@example.com", "Female", 25, 49, 160);
+
+    manager->deleteUserProfile("hi@example.com");
+
+
 }
 
 MainWindow::~MainWindow()
 {
+    delete manager;
     delete ui;
 }
 
--- RaDoTech/mainwindow.h
+++ RaDoTech/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "UserProfileManager.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,6 +17,7 @@ public:
     ~MainWindow();
 
 private:
+    UserProfileManager* manager;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
