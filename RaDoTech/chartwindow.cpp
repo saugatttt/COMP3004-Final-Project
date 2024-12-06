6a4cf5380a375c2fa4fbf12b31f7f48c01fc0831
Wereowen <owenchanchaktao@gmail.com>
2024-12-05 23:52:23 -0500

    Changed chart format

--- RaDoTech/chartwindow.cpp
+++ RaDoTech/chartwindow.cpp
@@ -18,34 +18,52 @@ ChartWindow::ChartWindow(QWidget *parent, QDate date, const QList<int>& measurem
     ui(new Ui::ChartWindow) {
     ui->setupUi(this);
 
-    QBarSeries* series = new QBarSeries();
-    QBarSet* set = new QBarSet("Measurements");
-    QList<QColor> barColors;
+    int numGroups = measurements.size() / 2; // Number of measurement groups (e.g., H1, H2, ..., F6)
 
-    for(int i = 0; i < measurements.size(); ++i) {
-        *set << measurements[i];
-        if(healthStatus[i] == 0) {
-            barColors.append(QColor(Qt::green));
-        }
-        else {
-            barColors.append(QColor(Qt::red));
-        }
+    // Create two QBarSets: one for Left (L) and one for Right (R)
+    QBarSet *setL = new QBarSet("Left (L)");
+    QBarSet *setR = new QBarSet("Right (R)");
+
+    // Lists to store bar colors based on healthStatus
+    QList<QColor> barColorsL;
+    QList<QColor> barColorsR;
+
+    // Populate the QBarSets and determine bar colors
+    for(int i = 0; i < numGroups; ++i) {
+        // Assign measurements
+        int measurementL = measurements[2 * i];     // L measurement
+        int measurementR = measurements[2 * i + 1]; // R measurement
+
+        *setL << measurementL;
+        *setR << measurementR;
+
+        // Assign colors based on healthStatus
+        int healthStatusL = healthStatus[2 * i];
+        int healthStatusR = healthStatus[2 * i + 1];
+
+        barColorsL.append(healthStatusL == 0 ? QColor(Qt::green) : QColor(Qt::red));
+        barColorsR.append(healthStatusR == 0 ? QColor(Qt::green) : QColor(Qt::red));
     }
-    series->append(set);
+
+    QBarSeries *series = new QBarSeries();
+    series->append(setL);
+    series->append(setR);
 
     QChart *chart = new QChart();
     chart->addSeries(series);
-    QString chartTitle = QString("Measurements on %1").arg(date.toString("yyyy-MM-dd"));
+    QString chartTitle = QString("Scan on %1").arg(date.toString("yyyy-MM-dd"));
     chart->setTitle(chartTitle);
     chart->setAnimationOptions(QChart::SeriesAnimations);
 
-     // H1 L, H1 R, H2 L, H2 R, ..., H6 L, H6 R, F1 L, F1 R, ..., F6 L, F6 R
+    // Custom categories for the X-axis (e.g., H1, H2, ..., F6)
     QStringList categories;
-    for(int i = 1; i <= 6; ++i) {
-        categories << QString("H%1 L").arg(i) << QString("H%1 R").arg(i);
-    }
-    for(int i = 1; i <= 6; ++i) {
-        categories << QString("F%1 L").arg(i) << QString("F%1 R").arg(i);
+    for(int i = 1; i <= numGroups; ++i) {
+        if(i <= 6) {
+            categories << QString("H%1").arg(i);
+        }
+        else {
+            categories << QString("F%1").arg(i - 6);
+        }
     }
 
     // x-axis
@@ -57,11 +75,16 @@ ChartWindow::ChartWindow(QWidget *parent, QDate date, const QList<int>& measurem
 
     // y-axis
     QValueAxis *axisY = new QValueAxis();
-    int maxMeasurement = *std::max_element(measurements.begin(), measurements.end());
-    axisY->setRange(0, maxMeasurement + 5); // Add some padding
+    if (!measurements.isEmpty()) {
+        int maxMeasurement = *std::max_element(measurements.begin(), measurements.end());
+        axisY->setRange(0, maxMeasurement + 5); // Add some padding
+    } else {
+        axisY->setRange(0, 10); // Default range if measurements are empty
+    }
     axisY->setTitleText("Ryodoraku Values");
     chart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisY);
+
 
     QFont titleFont;
     titleFont.setPixelSize(18);
